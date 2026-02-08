0c3861eef4cc5856efe6fd84ee633bedf64eab2b
gengen0719 <74813858+gengen0719@users.noreply.github.com>
2026-02-08 15:34:16 +0900

    Normalize start time in LiveCard

--- front/src/components/LiveCardContainer.tsx
+++ front/src/components/LiveCardContainer.tsx
@@ -5,6 +5,8 @@ type LiveData = {
     id: string;
     place: string;
     date: string;
+    startTime?: string;
+    start_time?: string;
     title: string;
     price: number;
     imgUrl: string;
@@ -18,7 +20,11 @@ function LiveCardContainer(){
         fetch("https://xbtsustt5e.execute-api.ap-northeast-1.amazonaws.com/dev/live-datas",{method: 'GET'})
         .then( (response) => response.json())
         .then( (data) => {
-            setLiveDatas(data.liveDatas); 
+            const normalizedLiveDatas = data.liveDatas.map((liveData: LiveData) => ({
+                ...liveData,
+                startTime: liveData.startTime ?? liveData.start_time,
+            }));
+            setLiveDatas(normalizedLiveDatas); 
         });
     }, [])
 
@@ -34,7 +40,16 @@ function LiveCardContainer(){
         <div className="live-card-container">
             {liveDatas.map( (liveData) => {
                 return (
-                    <LiveCard key={liveData.id} id={liveData.id} title={liveData.title} place={liveData.place} date={liveData.date} price={liveData.price} img={liveData.imgUrl}  />
+                    <LiveCard
+                        key={liveData.id}
+                        id={liveData.id}
+                        title={liveData.title}
+                        place={liveData.place}
+                        date={liveData.date}
+                        startTime={liveData.startTime}
+                        price={liveData.price}
+                        img={liveData.imgUrl}
+                    />
                 )
             })}
         </div>
